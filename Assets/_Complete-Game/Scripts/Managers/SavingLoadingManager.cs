144203fff20fd888a974c5b0b7df4fd18c1adc05
hiszpanskainkwizycja <hiszpanskainkwizycja@gmail.com>
2019-03-03 15:15:52 +0100

    SavingLoadingManager, GameSave stub.

---
+++ Assets/_Complete-Game/Scripts/Managers/SavingLoadingManager.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+using UnityEngine;
+
+namespace CompleteProject
+{
+    public class SavingLoadingManager : MonoBehaviour
+    {
+        string SavePath => Application.persistentDataPath + "/QuickSave.sav";
+
+        public void SaveGame()
+        {
+            var save = new GameSave(); 
+            WriteToFile(save);
+        }
+
+        public void LoadGame()
+        {
+            var save = ReadFromFile();
+            Debug.Log(save.x);
+        }
+
+        void WriteToFile(GameSave save)
+        {
+            using (var stream = File.Open(SavePath, FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                new XmlSerializer(typeof(GameSave)).Serialize(stream, save);
+            }
+        }
+
+        GameSave ReadFromFile()
+        {
+            using (var stream = File.Open(SavePath, FileMode.Open, FileAccess.Read))
+            {
+                return new XmlSerializer(typeof(GameSave)).Deserialize(stream) as GameSave;
+            }
+        }
+
+        [ContextMenu("TestSaveLoad")]
+        public void TestSaveLoad()
+        {
+            SaveGame();
+            LoadGame();
+        }
+    }
+}
---
+++ Assets/_Complete-Game/Scripts/SavingLoading/GameSave.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+[Serializable]
+public class GameSave
+{
+    //for testing purposes
+    public int x = 5;
+}
